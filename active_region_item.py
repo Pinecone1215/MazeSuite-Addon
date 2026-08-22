4e2b61401e3f5b73b5a44b1286a38329a7e0b4b2
Pinecone1215 <pinecone1215@gmail.com>
2026-08-22 13:57:52 +0800

    refactor: add RegionItem base class
    - Added a shared RegionItem base class.
    - Moved common geometry and Item behavior into RegionItem.
    - Reduced duplication between ActiveRegionItem and EndRegionItem.
    - Preserved separate Region types for future extension.

--- active_region_item.py
+++ active_region_item.py
@@ -5,19 +5,8 @@ Created on Sat Aug 22 01:03:54 2026
 @author: Pinecone
 """
 
-from PySide6.QtCore import QPointF, QRectF
-from PySide6.QtWidgets import QGraphicsRectItem, QGraphicsItem
+from region_item import RegionItem
 
-class ActiveRegionItem(QGraphicsRectItem):
+class ActiveRegionItem(RegionItem):
     def __init__(self, x1, y1, x2, y2):
-        '''
-        把兩個座標點轉成 QPointF，再建立 QRectF 並 normalized()，
-        因此不論使用者往哪個方向拖曳，最後都會得到正常的矩形
-        '''
-        p1, p2 = QPointF(x1, y1), QPointF(x2, y2)
-        rect = QRectF(p1, p2).normalized()
-        
-        super().__init__(rect)
-
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
-        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+        super().__init__(x1, y1, x2, y2)
---
+++ region_item.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 13:51:15 2026
+
+@author: Pinecone
+"""
+
+from PySide6.QtCore import QPointF, QRectF
+from PySide6.QtWidgets import QGraphicsRectItem, QGraphicsItem
+
+class RegionItem(QGraphicsRectItem):
+    def __init__(self, x1, y1, x2, y2):
+        '''
+        把兩個座標點轉成 QPointF，再建立 QRectF 並 normalized()，
+        因此不論使用者往哪個方向拖曳，最後都會得到正常的矩形
+        '''
+        p1, p2 = QPointF(x1, y1), QPointF(x2, y2)
+        rect = QRectF(p1, p2).normalized()
+        
+        super().__init__(rect)
+
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
